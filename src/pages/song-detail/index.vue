297af9d1e48cdbb4fc796e8c9c9136989541e4b1
Alisa <alisalicn@gmail.com>
2019-03-08 15:22:58 +0800

    feat: lyric scroll and progress bat

--- src/pages/song-detail/index.vue
+++ src/pages/song-detail/index.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="song" v-if="songDetail">
     <div class="song-bg" :style="{backgroundImage: 'url(' + songDetail.al.picUrl + ')'}">
-      <div class="song-wrapper">
-        <div class="song-disc">
+    </div>
+    <div class="song-wrapper" @click="toggleLyricVisible">
+      <!--<div class="song-name">-->
+      <!--{{ songDetail.name }}-->
+      <!--<span v-if="songDetail.alia.length">({{ songDetail.alia[0] }})</span>-->
+      <!--</div>-->
+      <div class="song-inner">
+        <div class="song-disc" v-if="!lyricVisible">
           <div class="song-disc-light"></div>
           <div class="song-disc-dark"></div>
           <div class="song-cover-wrapper">
-            <div class="song-cover" :class="{rotate: isPlaying}" v-if="songDetail && songDetail.al" @click="togglePlay">
+            <div class="song-cover" :class="{rotate: isPlaying}" v-if="songDetail && songDetail.al">
               <img class="song-cover-img" :src="songDetail.al.picUrl">
-              <div class="play-icon" v-if="!isPlaying">
-                <i-icon type="playon" size="40" color="#eee"/>
-              </div>
             </div>
           </div>
         </div>
-        <div class="song-info">
-          <div class="song-name">
-            {{ songDetail.name }}
-            <span v-if="songDetail.alia.length">({{ songDetail.alia[0] }})</span>
-          </div>
-          <div class="song-lyric" v-if="lyric">
-            <div v-for="(lyric,index) in lyric.lines" :key="index" :class="{'current': index === lyricCurrentLine}">
-              {{ lyric.txt }}
-            </div>
+        <div class="song-lyric" v-else-if="lyricVisible && lyric"
+             :style="{top: lyricTop + 'px', transition: 'all 1s ease'}">
+          <div v-for="(lyric,index) in lyric.lines" :key="index" :class="{'current': index === lyricCurrentLine}">
+            {{ lyric.txt }}
           </div>
         </div>
+      </div>
+    </div>
+    <div class="song-play-info">
+      <div class="song-progress">
+        <!--<input type="range" min="0" max="100" v-model="playProgress">-->
+        <!--<input type="range" min="0" max="100" value="90">-->
+        <div class="bar-bg">
+          <div class="bar-inner" :style="{width: playProgress + '%'}"></div>
+          <div class="bar-point"></div>
+        </div>
+      </div>
+      <div class="song-control">
+        <i-icon type="refresh" size="26" color="#fff"/>
+        <i-icon type="return" size="26" color="#fff"/>
+        <i-icon :type="isPlaying ? 'suspend' : 'playon'" size="26" color="#fff" @click="togglePlay"/>
+        <i-icon type="enter" size="26" color="#fff"/>
+        <i-icon type="other" size="26" color="#fff"/>
       </div>
     </div>
   </div>
@@ -41,8 +56,11 @@ export default {
       songDetail: null,
       innerAudioContext: null,
       isPlaying: false,
+      playProgress: 0,
+      lyricVisible: false,
       lyric: null,
       lyricCurrentLine: 0,
+      lyricTop: 0,
     }
   },
   onLoad(option) {
@@ -66,6 +84,10 @@ export default {
         this.innerAudioContext.autoplay = false
         this.innerAudioContext.src = this.songUrl
         this.innerAudioContext.onPlay(() => {
+        })
+        this.innerAudioContext.onTimeUpdate(() => {
+          this.playProgress = this.innerAudioContext.currentTime / this.innerAudioContext.duration * 100
+          console.log('progress: ', this.playProgress)
         })
         this.innerAudioContext.onPause(() => {
         })
@@ -98,7 +120,18 @@ export default {
     },
     handleLyric({ lineNum, txt }) {
       this.lyricCurrentLine = lineNum
+      if (lineNum > 6) {
+        this.lyricTop = (lineNum - 6) * -18
+      } else {
+        this.lyricTop = 0
+      }
     },
+    toggleLyricVisible() {
+      this.lyricVisible = !this.lyricVisible
+    },
+  },
+  onUnload() {
+    this.innerAudioContext.destroy()
   },
 }
 </script>
@@ -111,7 +144,7 @@ export default {
   background-size auto 100%
   transform scale(1.5)
   transform-origin center top
-  position fixed
+  position absolute
   left 0
   right 0
   top 0
@@ -132,10 +165,14 @@ export default {
   position absolute
   top 0
   right 0
-  bottom 0
+  bottom 100px
   left 0
   z-index 10
-  padding-top 40px
+  .song-inner
+    position relative
+    margin-top 30px
+    height calc(100% - 30px)
+    overflow hidden
 
 vendors = official
 @keyframes rotates {
@@ -149,9 +186,9 @@ vendors = official
 
 .song-disc
   position relative
-  width 180px
-  height 180px
-  margin 0 auto
+  width 200px
+  height 200px
+  margin 100px auto
   .song-disc-light
     position absolute
     top 0
@@ -197,26 +234,51 @@ vendors = official
       &.rotate
         animation: rotates 20s linear infinite;
 
-.song-info
+.song-name
+  color #fff
+  font-size 14px
+  text-align center
+  overflow hidden
+  text-overflow ellipsis
+  white-space nowrap
   width 200px
-  margin 20px auto
+
+.song-lyric
+  position absolute
+  left 50px
+  right 50px
   display flex
   flex-direction column
   justify-content center
-  .song-name
-    color #fff
-    font-size 14px
-    text-align center
-    overflow hidden
-    text-overflow ellipsis
-    white-space nowrap
-    width 200px
-
-.song-lyric
   color rgba(255, 255, 255, .6)
-  font-size 10px
-  line-height 1.5
+  font-size 12px
+  line-height 1.8
   text-align center
   .current
     color #fff
+
+.song-play-info
+  position absolute
+  left 0
+  right 0
+  bottom 0
+  height 80px
+  z-index 10
+  .song-control
+    display flex
+    justify-content space-around
+    padding 10px 0
+
+$progress-bar-height = 4px
+.song-progress
+  .bar-bg
+    position relative
+    height $progress-bar-height
+    background-color #ccc
+    .bar-inner
+      position absolute
+      top 0
+      left 0
+      height $progress-bar-height
+      background-color $color-primary
 </style>
